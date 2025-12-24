7d7325ce25ae66386fbfae7a134d0cee651df756
Aman <ak5654707@gmail.com>
2025-12-24 22:08:42 +0530

    added sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -1,9 +1,43 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://airoxe.in';
+
   return [
     {
-      url: 'https://airoxe.in',
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/products`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/products/airoxe-airshield`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/products/active-mask`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/refund`,
       lastModified: new Date(),
     },
   ];
